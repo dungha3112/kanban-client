97980b00bd9bf59cadac89fdafca8b4480accc7a
dungha3112 <ktdung113@gmail.com>
2024-03-20 15:23:13 +0700

    Add env variable

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:7819/api/v1/";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? process.env.REACT_APP_API_URL
+    : process.env.REACT_APP_DEV_API_URL;
 
 // ------------------------------------------------
 // ------------------------------------------------
